8973d619b5b1884042123a1c4e1af0011331923e
Kevin Raneri <kevin.raneri@gmail.com>
2021-11-17 12:13:19 -0500

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.14"
+    id("io.papermc.paperweight.patcher") version "1.2.0"
 }
 
 repositories {
