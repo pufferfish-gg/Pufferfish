fd44f68d195f2cf7ae2e57b1562a97b58bec439e
Kevin Raneri <kevin.raneri@gmail.com>
2023-11-02 07:59:30 -0500

    Update paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.8"
+    id("io.papermc.paperweight.patcher") version "1.5.9"
 }
 
 repositories {
