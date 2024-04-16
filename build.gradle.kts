852ea89d9eaa3af12ace8839275fb8116fc98b08
Kevin Raneri <kevin.raneri@gmail.com>
2024-04-16 00:56:01 -0500

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@00fd87a Return dummy string instead of empty optional
    PaperMC/Paper@710dced [ci skip] move custom brig exception to paper package
    PaperMC/Paper@45d1486 build: Update paperweight to 1.5.12 and Gradle Wrapper to 8.7 (#10361)
    PaperMC/Paper@e709245 Add config option for tripwire disarming fix
    PaperMC/Paper@a203544 build: Compile against and shade the filtered jar (#9747)
    PaperMC/Paper@bd38e03 Updated Upstream (Bukkit/CraftBukkit) (#10379)
    PaperMC/Paper@a774fba feat: Entity#teleportAsync method with TeleportFlags (#10371)
    PaperMC/Paper@06361fa Fix invalid block entities created during world gen (#10375)
    PaperMC/Paper@bbee11f Deprecate Bukkit#getLogger (#10388)
    PaperMC/Paper@d8456ee Don't throw NPE for unplaced blockstate on #getDrops (#10366)
    PaperMC/Paper@182e79b Add more item use API (#10304)
    PaperMC/Paper@acf838f Backport some stuff from the generators branch (#10365)
    PaperMC/Paper@3d31e45 Add BlockBreakProgressUpdateEvent (#10300)
    PaperMC/Paper@8e75001 Disable vertical air friction when item entities have friction disabled (#10369)
    PaperMC/Paper@241d8e2 Ignore minecart in activation range (#10359)
    PaperMC/Paper@1207162 Allow player-list API to self un/list (#10358)
    PaperMC/Paper@5436d44 Deprecate several Keyed#getKey methods (#10357)
    PaperMC/Paper@a7f1dc6 Change online mode default for Velocity configuration (#10413)
    PaperMC/Paper@37db2d7 [ci skip] Update book page/char limit for book meta doc (#10415)
    PaperMC/Paper@526795b Update patches to handle vineflower decompiler (#10406)
    PaperMC/Paper@8fe90de [ci skip] Referenced InventoryDragEvent in documentation of InventoryClickEvent (#10395)
    PaperMC/Paper@46d462b Fix StackOverflowException thrown on shutdown (Fixes #10404) (#10408)
    PaperMC/Paper@f061e76 Fix hit criteria advancement triggered before changing state (#10409)
    PaperMC/Paper@3263470 Add color transition and clone functions to ParticleBuilder (#10342)
    PaperMC/Paper@4445d23 Deprecate ItemStack#setType & add ItemStack#withType (#10290)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.11"
+    id("io.papermc.paperweight.patcher") version "1.5.15"
 }
 
 repositories {
@@ -14,8 +14,8 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    remapper("net.fabricmc:tiny-remapper:0.10.1:fat")
+    decompiler("org.vineflower:vineflower:1.11.0-20240412.144930-14")
     paperclip("io.papermc:paperclip:3.0.3")
 }
 
